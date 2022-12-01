3b9235c39af4d50a81e72a4ccce301cc4b2b5374
Antonio Junior <junyfla@gmail.com>
2022-12-01 03:28:28 -0300

    Reparo e criação de projeto POO

--- src/Dev.java
+++ src/Dev.java
@@ -1,4 +1,15 @@
 
 public class Dev {
 
+    public String getConteudosIncritos() {
+        return null;
+    }
+
+    public String getConteudosConcluidos() {
+        return null;
+    }
+
+    public void progredir() {
+    }
+
 }
